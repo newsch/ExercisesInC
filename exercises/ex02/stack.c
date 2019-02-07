e2e1028588fc94fec78fbf2b3bfb0486ac326d32
Evan New-Schmidt <evan@new-schmidt.com>
2019-02-07 00:03:26 -0500

    Finish ex02 stack q3

--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -45,3 +45,19 @@ int main()
 
     return 0;
 }
+
+/* ex02 answers
+
+1. Program prints the locations of two arrays defined locally in functions, and then prints the contents of the arrays locations in memory (presumably the same space, with the contents of the second array overwriting the contents of the first array after deallocation).
+
+2. On compilation, I get the following warning:
+  ```
+  stack.c: In function ‘foo’:
+  stack.c:22:12: warning: function returns address of local variable [-Wreturn-local-addr]
+       return array;
+              ^
+  ```
+  It doesn't like returning local variable locations to code with different scope.
+
+3. On execution, I see the two addresses of the arrays and no other output - not even empty line breaks.
+*/
