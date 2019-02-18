f81d73a9a3902a65a57d74234d38a027b9b51e2b
Evan New-Schmidt <evan@new-schmidt.com>
2019-02-18 16:32:40 -0500

    Implement argument parsing

---
+++ exercises/ex03/notes.md
@@ -0,0 +1,14 @@
+
+`tee [ -a ] [ -i ] [ File ... ]`
+
+General plan for basics:
+- open file in write mode, else error
+- read (an amount?) from stdin, write to file, write to stdout
+- append: open file in append mode vs write
+- ignore interrupt signals: which ones? in python you can catch the error.
+
+cmdline interface options:
+- [getopt](https://www.gnu.org/software/libc/manual/html_node/Getopt.html): recursive, bash-ish method, standard POSIX if only short-options
+- [argp](https://www.gnu.org/software/libc/manual/html_node/Argp.html): python `argparse`-ish, more features (automatic help/version)
+- ISO C: `int main (int argc, char *argv[])`
+- UNIX (non-POSIX): `int main (int argc, char *argv[], char *envp[])`
---
+++ exercises/ex03/shtee.c
@@ -0,0 +1,66 @@
+#include <argp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const char* argp_program_version = "shtee alpha-0.1";
+const char* argp_program_bug_address = "evan@new-schmidt.com";
+
+static char doc[] = "shtee -- a tee clone";
+static char args_doc[] = "[FILE1]...";
+
+// commandline options
+static struct argp_option options[] = {
+    {"append", 'a', 0, OPTION_ARG_OPTIONAL, "append to the given FILEs, do not overwrite"},
+    {"ignore-interrupts", 'i', 0, OPTION_ARG_OPTIONAL, "ignore interrupt signals"},
+    {"foobar", 0, 0, OPTION_DOC, "neat"},
+    { 0 }  // terminating entry
+};
+
+// internal representation
+struct arguments {
+    int append, ignoreInterrups;  // flags
+};
+
+// parse an option
+static error_t parse_opt(int key, char* arg, struct argp_state* state) {
+    // extract arguments from state
+    struct arguments *arguments = state->input;
+
+    switch (key) {
+        case 'a':
+            arguments->append = 1;
+            break;
+        case 'i':
+            arguments->ignoreInterrups = 1;
+            break;
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc };
+
+int main(int argc, char* argv[]) {
+    struct arguments arguments;
+    arguments.append = 0;
+    arguments.ignoreInterrups = 0;
+
+    // print argc and argv
+    printf("argc: %i, argv: [", argc);
+    for (int i = 0; i<argc; i++) {
+        printf("\"%s\"", argv[i]);
+        if (i != argc-1) {
+            printf(",");
+        }
+    }
+    printf("]\n");
+
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+    // print state
+    printf ("append = %s\nignoreInterrupts = %s\n",
+          arguments.append ? "yes" : "no",
+          arguments.ignoreInterrups ? "yes" : "no");
+
+    // TODO: implement tee
+}
