63803edc3f6b2f485db3f9203a1865f0725c6561
Evan New-Schmidt <evan@new-schmidt.com>
2019-03-07 19:05:53 -0500

    Finish ex05

--- exercises/ex05/find_track_oo.c
+++ exercises/ex05/find_track_oo.c
@@ -30,8 +30,12 @@ typedef regex_t Regex;
 * returns: new Regex
 */
 Regex *make_regex(char *pattern, int flags) {
-    // FILL THIS IN!
-    return NULL;
+    regex_t* regex = malloc(sizeof(regex_t));
+    if (regcomp(regex, pattern, flags) != 0) {
+        fprintf(stderr, "Could not compile regex\n");
+        exit(1);
+    }
+    return regex;
 }
 
 /* Checks whether a regex matches a string.
@@ -41,8 +45,12 @@ Regex *make_regex(char *pattern, int flags) {
 * returns: 1 if there's a match, 0 otherwise
 */
 int regex_match(Regex *regex, char *s) {
-    // FILL THIS IN!
-    return 0;
+    int ret = regexec(regex, s, 0, NULL, 0);
+    if (ret) {
+        return 0;
+    } else {
+        return 1;
+    }
 }
 
 /* Frees a Regex.
@@ -50,7 +58,8 @@ int regex_match(Regex *regex, char *s) {
 * regex: Regex pointer
 */
 void regex_free(Regex *regex) {
-    // FILL THIS IN!
+    regfree(regex);
+    free(regex);
 }
 
 
