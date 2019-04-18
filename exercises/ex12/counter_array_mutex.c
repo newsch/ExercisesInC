14adeaacfa1b37fb456f4666fbae9ac799362f60
Evan New-Schmidt <evan@new-schmidt.com>
2019-04-18 19:55:31 -0400

    Time implementations

--- exercises/ex12/counter_array_mutex.c
+++ exercises/ex12/counter_array_mutex.c
@@ -75,7 +75,7 @@ void join_thread(pthread_t thread)
 
 void child_code(Shared *shared)
 {
-    printf("Starting child at counter %d\n", shared->counter);
+    // printf("Starting child at counter %d\n", shared->counter);
 
     while (1) {
         if (shared->counter >= shared->end) {
@@ -86,7 +86,7 @@ void child_code(Shared *shared)
         shared->counter++;
 
         if (shared->counter % 10000 == 0) {
-            printf("%d\n", shared->counter);
+            // printf("%d\n", shared->counter);
         }
         mutex_unlock(shared->mutex);
     }
@@ -96,7 +96,7 @@ void *entry(void *arg)
 {
     Shared *shared = (Shared *) arg;
     child_code(shared);
-    printf("Child done.\n");
+    // printf("Child done.\n");
     pthread_exit(NULL);
 }
 
@@ -104,12 +104,12 @@ void check_array(Shared *shared)
 {
     int i, errors=0;
 
-    printf("Checking...\n");
+    // printf("Checking...\n");
 
     for (i=0; i<shared->end; i++) {
         if (shared->array[i] != 1) errors++;
     }
-    printf("%d errors.\n", errors);
+    // printf("%d errors.\n", errors);
 }
 
 int main()
